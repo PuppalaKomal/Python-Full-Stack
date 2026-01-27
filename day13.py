c749141cdee8bd2edc419205f40411fa767b86d4
PuppalaKomal <komalpuppala@gmail.com>
2026-01-27 08:01:07 +0530

    initial commit

--- day13.py
+++ day13.py
@@ -14,7 +14,7 @@ n=int(input("Enter the number: "))
 def print_table(n):
     for i in range(1,21):
         print(f"{n} * {i} ={n*i}")
-print_table(n)"""
+print_table(n)
 
 
 #printing patterns using nested loops 
@@ -79,22 +79,23 @@ for i in range(5):
             print("*",end=" ")
         else:
             print(" ",end=" ")
-    print()
+    print()"""
+    
 #  * * * * * *     
 #  *
 #  * * * * * *
 #            *
 #  * * * * * *
-n = 6
+n = 7
 
 for i in range(n):
     for j in range(n):
         if (
             i == 0 or                      # top row
             i == n - 1 or                  # bottom row
-            (i == 5 // 2) or               # middle row
+            (i == 6 // 2) or               # middle row
             (j == 0 and i <= 5 // 2) or    # left vertical (top half)
-            (j == n - 1 and i >= 5 // 2)   # right vertical (bottom half)
+            (j == n - 1 and i >= 6 // 2)   # right vertical (bottom half)
         ):
             print("*", end=" ")
         else:
