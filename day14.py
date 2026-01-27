cc83888e4c43e697a1bba27bdea15fda72216531
PuppalaKomal <komalpuppala@gmail.com>
2026-01-27 18:47:26 +0530

    initial commit

--- day14.py
+++ day14.py
@@ -2,7 +2,7 @@
 # 5 6 7 8
 # 9 10 11 12
 # 13 14 15 16
-"""num=4
+num=4
 n= 1
 for i in range(num):
     for j in range(num):
@@ -100,7 +100,7 @@ for i in range(n):
             print("*",end="\t")
         else:
             print(" ",end="\t")
-    print()"""
+    print()
 
 #  * * * *
 #    *   *                                      
